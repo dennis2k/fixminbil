b02bc9a535be70f8c8e974a939d2d972f17a77aa
Dennis Mortensen <stinarn@gmail.com>
2019-12-19 14:32:10 +0100

    new build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,12 @@
     <!-- Menu -->
     <Menu />
     <!-- Splash text -->
-    <div class="w-full mt-24 md:mt-0 home bg-cover md:h-1080 md:overflow-hidden">
+    <div class="w-full mt-28 md:mt-0 home bg-cover md:h-1080 md:overflow-hidden">
       <div class="md:py-300p py-10 w-full max-w-container flex justify-center mx-auto">
         <div class="md:text-7xl text-3xl font-semibold">
           <h2 class="text-blue-400">Service og reparation</h2>
-          <div class="text-white" id="service-mobile">af alle mærker</div>
-          <div class="text-white text-xl md:text-4xl uppercase pt-4 italic">- 20 års erfaring</div>
+          <div class="text-white" id="service-mobile">af alle bilmærker</div>
+          <div class="text-white text-xl md:text-4xl uppercase pt-4 italic">- 40 års erfaring</div>
         </div>
       </div>
     </div>
@@ -16,12 +16,16 @@
     <!-- Services mobile -->
     <div style="margin-top: -20px"></div>
     <div class="block md:hidden py-6 w-full">
-      <div class="text-3xl w-full text-center" >Services</div>
+      <div class="text-3xl w-full text-center">Services</div>
+      <div
+        class="py-4 px-4"
+      >E. Johansens Autoelektro er din specialiserede mekaniker i Grindsted og omegn. Her får du fuld service og diagnose af bilens elektronik med avanceret testudstyr. Vi fixer også ethvert mekanisk problem – selv de lidt mere komplicerede af slagsen.</div>
       <div class="py-4 px-4">
-        E. Johansens Autoelektro er din specialiserede mekaniker i Grindsted og omegn. Her får du fuld service og diagnose af bilens elektronik med avanceret testudstyr. Vi fixer også ethvert mekanisk problem – selv de lidt mere komplicerede af slagsen.
-      </div>
-      <div class="py-4 px-4">
-        <div v-for="(item, index) in services" :key="item" :id="index === services.length -2 ? 'contact-mobile' : 'a'+index">
+        <div
+          v-for="(item, index) in services"
+          :key="item"
+          :id="index === services.length -2 ? 'contact-mobile' : 'a'+index"
+        >
           <i class="fa fa-check text-green-400 mr-4" />
           {{item}}
         </div>
@@ -30,7 +34,6 @@
 
     <!-- Kontakt Mobile -->
     <div
-    
       class="w-full md:bg-cover contact grayscale flex items-center sm:hidden"
       :class="{'darken': isContactInFocus}"
     >
@@ -38,7 +41,11 @@
         <div class="bg-white p-6 w-full my-8">
           <div class="text-3xl">Kontakt</div>
           <div class="mb-2 mt-2">
-            Hvis du ønsker at vide en pris på opgaven på din bil, så kontakt Flemming allerede i dag, og få et uforpligtende tilbud. Du kan bestille tid på telefon <a class="text-blue-700" href="tel:+4575320566">75320566</a> eller udfyld kontaktformularen.
+            Hvis du ønsker at vide en pris på opgaven på din bil, så kontakt Flemming allerede i dag, og få et uforpligtende tilbud. Du kan bestille tid på telefon
+            <a
+              class="text-blue-700"
+              href="tel:+4575320566"
+            >75320566</a> eller udfyld kontaktformularen.
           </div>
           <div class="w-full">
             <div class>
@@ -76,7 +83,14 @@
                 </div>
               </div>
               <div class="w-full flex justify-end">
-                <button class="px-4 py-2 bg-blue-600 text-white mt-4 hover:bg-blue-400">Send</button>
+                <div class="pr-2 flex-1" v-if="successMessage || errorMessage">
+                  <div v-if="successMessage" class="text-green-600">{{successMessage}}</div>
+                  <div v-if="errorMessage" class="text-red-600">{{errorMessage}}</div>
+                </div>
+                <button
+                  class="px-4 py-2 bg-blue-600 text-white mt-4 hover:bg-blue-400"
+                  @click="sendMail"
+                >Send</button>
               </div>
             </div>
           </div>
@@ -88,11 +102,19 @@
     <div class="w-full flex flex-col items-center pt-20p pb-40p md:hidden">
       <div class="text-3xl" id="about-mobile">Om</div>
       <div class="pt-3 px-4 w-full">
-        Hvis du er på udkig efter en mekaniker med erfaring, og ønsker at din bil er i trygge hænder, så kig forbi E. Johansens Autoelektro i Grindsted.<br /> 
-        Indehaveren af E. Johansens Autoelektro, Flemming Kristensen, har mere end 40 års erfaring som mekaniker. <br /> Han udfører alle typer opgaver – uanset bilmærke. Få fixet din bil hos E. Johansens Autoelektro og bliv godt kørende med en kvalitetsbevidst og professionel service af din bil. 
+        Hvis du er på udkig efter en mekaniker med erfaring, og ønsker at din bil er i trygge hænder, så kig forbi E. Johansens Autoelektro i Grindsted.
+        <br />Indehaveren af E. Johansens Autoelektro, Flemming Kristensen, har mere end 40 års erfaring som mekaniker.
+        <br />Han udfører alle typer opgaver – uanset bilmærke. Få fixet din bil hos E. Johansens Autoelektro og bliv godt kørende med en kvalitetsbevidst og professionel service af din bil.
       </div>
       <div class="pt-2">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10680.076639074025!2d8.971584964698524!3d55.75486211407749!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2db25eb9fc047884!2sE.%20Johansens%20Autoelektro%20v%2FFlemming%20Kristensen!5e0!3m2!1sen!2ses!4v1575067667119!5m2!1sen!2ses" width="320" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10680.076639074025!2d8.971584964698524!3d55.75486211407749!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2db25eb9fc047884!2sE.%20Johansens%20Autoelektro%20v%2FFlemming%20Kristensen!5e0!3m2!1sen!2ses!4v1575067667119!5m2!1sen!2ses"
+          width="320"
+          height="300"
+          frameborder="0"
+          style="border:0;"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
 
@@ -109,7 +131,7 @@
         </div>
         <div class="h-300p w-300p img-hover-zoom--quick-zoom overflow-hidden">
           <img
-            src="https://res.cloudinary.com/dv6mzgcxx/image/upload/c_fit,h_300,q_65,w_300/v1573933638/frontcar.d8b8d784_nbfhyn.png"
+            src="https://res.cloudinary.com/dv6mzgcxx/image/upload/c_fit,h_300,w_300/v1576755636/croppedflemming_ppqs0r.jpg"
             class="w-300p h-300p"
           />
         </div>
@@ -131,12 +153,16 @@
           <div class="uppercase text-xl pt-6 italic tracking-wider">til en venlig pris!</div>
         </div>
         <div class="h-300p w-300p img-hover-zoom--quick-zoom overflow-hidden">
-          <img src="../assets/hands.png" class="w-300p h-300p" />
+          <img
+            src="https://res.cloudinary.com/dv6mzgcxx/image/upload/c_fit,h_300,w_300/v1576756092/hands_dlr7nr.jpg"
+            class="w-300p h-300p"
+          />
         </div>
       </div>
     </div>
     <!-- Kontakt desktop -->
-    <div  id="contact-desktop"
+    <div
+      id="contact-desktop"
       class="w-full bg-contain md:bg-cover services grayscale md:flex items-center hidden"
       :class="{'darken': isContactInFocus}"
       style="height: 800px"
@@ -148,9 +174,13 @@
           @mouseleave="isContactInFocus = false"
         >
           <div class="text-3xl">Kontakt</div>
-          <div
-            class="mb-10 mt-6"
-          >Hvis du ønsker at vide en pris på opgaven på din bil, så kontakt Flemming allerede i dag, og få et uforpligtende tilbud. Du kan bestille tid på telefon <a class="text-blue-700" href="tel:+4575320566">75320566</a> eller udfyld kontaktformularen.</div>
+          <div class="mb-10 mt-6">
+            Hvis du ønsker at vide en pris på opgaven på din bil, så kontakt Flemming allerede i dag, og få et uforpligtende tilbud. Du kan bestille tid på telefon
+            <a
+              class="text-blue-700"
+              href="tel:+4575320566"
+            >75320566</a> eller udfyld kontaktformularen.
+          </div>
           <div class="w-full">
             <div class>
               <div class="input-container mt-2" :class="{'-not-empty': name}">
@@ -187,7 +217,14 @@
                 </div>
               </div>
               <div class="w-full flex justify-end">
-                <button class="px-4 py-2 bg-blue-600 text-white mt-4 hover:bg-blue-400">Send</button>
+                <button
+                  class="px-4 py-2 bg-blue-600 text-white mt-4 hover:bg-blue-400"
+                  @click="sendMail"
+                >Send</button>
+              </div>
+              <div class="pt-4" v-if="successMessage || errorMessage">
+                <div v-if="successMessage" class="text-green-600">{{successMessage}}</div>
+                <div v-if="errorMessage" class="text-red-600">{{errorMessage}}</div>
               </div>
             </div>
           </div>
@@ -196,7 +233,7 @@
     </div>
     <!-- Services Desktop -->
     <div class="w-full md:flex flex-col items-center py-40p hidden" id="service-desktop">
-      <div class="text-7xl mb-20p" >Services</div>
+      <div class="text-7xl mb-20p">Services</div>
       <div
         class="py-4 px-4 w-1100p mx-auto text-lg"
       >E. Johansens Autoelektro er din specialiserede mekaniker i Grindsted og omegn. Her får du fuld service og diagnose af bilens elektronik med avanceret testudstyr. Vi fixer også ethvert mekanisk problem – selv de lidt mere komplicerede af slagsen.</div>
@@ -212,35 +249,43 @@
     <div class="w-full md:flex flex-col items-center py-40p hidden">
       <div class="text-7xl mb-20p" id="about-desktop">Om</div>
       <div class="py-4 px-4 w-1100p mx-auto text-lg">
-          Hvis du er på udkig efter en mekaniker med erfaring, og ønsker at din bil er i trygge hænder, så kig forbi E. Johansens Autoelektro i Grindsted.<br /> 
-        Indehaveren af E. Johansens Autoelektro, Flemming Kristensen, har mere end 40 års erfaring som mekaniker. <br /> Han udfører alle typer opgaver – uanset bilmærke. Få fixet din bil hos E. Johansens Autoelektro og bliv godt kørende med en kvalitetsbevidst og professionel service af din bil. 
+        Hvis du er på udkig efter en mekaniker med erfaring, og ønsker at din bil er i trygge hænder, så kig forbi E. Johansens Autoelektro i Grindsted.
+        <br />Indehaveren af E. Johansens Autoelektro, Flemming Kristensen, har mere end 40 års erfaring som mekaniker.
+        <br />Han udfører alle typer opgaver – uanset bilmærke. Få fixet din bil hos E. Johansens Autoelektro og bliv godt kørende med en kvalitetsbevidst og professionel service af din bil.
       </div>
       <div class="pt-2">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10680.076639074025!2d8.971584964698524!3d55.75486211407749!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2db25eb9fc047884!2sE.%20Johansens%20Autoelektro%20v%2FFlemming%20Kristensen!5e0!3m2!1sen!2ses!4v1575067667119!5m2!1sen!2ses" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10680.076639074025!2d8.971584964698524!3d55.75486211407749!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2db25eb9fc047884!2sE.%20Johansens%20Autoelektro%20v%2FFlemming%20Kristensen!5e0!3m2!1sen!2ses!4v1575067667119!5m2!1sen!2ses"
+          width="600"
+          height="450"
+          frameborder="0"
+          style="border:0;"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
 
-<!-- Footer mobile -->
-      <div class="w-full flex justify-between py-2 px-4 bg-gray-900 md:hidden text-white">
-            <div>Hverdage 08 - 16.30</div>
-            <div>
-                <i class="fas fa-phone-alt mr-3"></i><a href="tel:+4575320566">75 32 05 66</a>
-            </div>
-        </div>
+    <!-- Footer mobile -->
+    <div class="w-full flex justify-between py-2 px-4 bg-gray-900 md:hidden text-white">
+      <div>Hverdage 08 - 16.30</div>
+      <div>
+        <i class="fas fa-phone-alt mr-3"></i>
+        <a href="tel:+4575320566">75 32 05 66</a>
+      </div>
+    </div>
 
-<!-- Footer desktop -->
+    <!-- Footer desktop -->
 
-          <div class="w-full hidden  md:flex flex-col py-2 px-4 bg-gray-800 text-white py-6">
-            <div class="w-1100p mx-auto flex justify-between">
-              <div>Hverdage 08 - 16.30</div>
-              <div>
-                  <i class="fas fa-phone-alt mr-3"></i><a href="tel:+4575320566">75 32 05 66</a>
-              </div>
-            </div>
-            <div class="w-1100p mx-auto">
-              Heimdalsvej 22, 7200 Grindsted, Denmark
-            </div>
+    <div class="w-full hidden md:flex flex-col py-2 px-4 bg-gray-800 text-white py-6">
+      <div class="w-1100p mx-auto flex justify-between">
+        <div>Hverdage 08 - 16.30</div>
+        <div>
+          <i class="fas fa-phone-alt mr-3"></i>
+          <a href="tel:+4575320566">75 32 05 66</a>
         </div>
+      </div>
+      <div class="w-1100p mx-auto">Heimdalsvej 22, 7200 Grindsted, Denmark</div>
+    </div>
   </div>
 </template>
 <script>
@@ -259,6 +304,8 @@ export default class Home extends Vue {
   body = "";
   isContactInFocus = false;
   pendingDelay = true;
+  errorMessage = "";
+  successMessage = "";
 
   services = [
     "Serviceeftersyn",
@@ -285,13 +332,48 @@ export default class Home extends Vue {
     await timerService.wait(200);
     this.pendingDelay = false;
   }
+
+  async sendMail() {
+    try {
+      this.successMessage = "";
+      this.errorMessage = "";
+      const response = await fetch(
+        "http://app.easylocalizer.com/api/v1/custom_send_email",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            name: this.name,
+            contact: this.phone,
+            body: this.body
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      this.phone = "";
+      this.name = "";
+      this.body = "";
+      const result = await response.json();
+      if (result.status === "OK") {
+        this.successMessage =
+          "Din forespørgsel er modtaget - du bliver kontakt så hurtigt som muligt :)";
+      } else {
+        this.errorMessage =
+          "Der skete en fejl - prøv igen, eller ring til 75320566";
+      }
+    } catch (error) {
+      this.errorMessage =
+        "Der skete en fejl - prøv igen, eller ring til 75320566";
+    }
+  }
 }
 </script>
 <style lang="less">
 .home {
   width: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url("https://res.cloudinary.com/dv6mzgcxx/image/upload/w_1920,ar_16:9,c_fill,g_auto,e_sharpen/v1573933642/car-service.8c32b1d9_e35frt.jpg");
+  background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url("https://res.cloudinary.com/dv6mzgcxx/image/upload/w_1920,e_art:incognito/v1576755067/IMG_5526_ygwj6a.jpg");
   background-repeat: no-repeat;
 }
 
@@ -299,7 +381,7 @@ export default class Home extends Vue {
   transition: background-image 0.3s ease;
   width: 100%;
   background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url("https://res.cloudinary.com/dv6mzgcxx/image/upload/c_fit,h_1080,w_1920/v1573935364/engine_va4amn.jpg");
+    url("https://res-console.cloudinary.com/dv6mzgcxx/thumbnails/transform/v1/image/upload//v1576758068/SU1HXzU1MjVfY2xvb3Ro/drilldown");
   background-repeat: no-repeat;
 
   &.darken {
@@ -307,7 +389,7 @@ export default class Home extends Vue {
         from(rgba(0, 0, 0, 0.5)),
         to(rgba(0, 0, 0, 0.8))
       ),
-      url("https://res.cloudinary.com/dv6mzgcxx/image/upload/c_fit,h_1080,w_1920/v1573935364/engine_va4amn.jpg");
+      url("https://res-console.cloudinary.com/dv6mzgcxx/thumbnails/transform/v1/image/upload//v1576758068/SU1HXzU1MjVfY2xvb3Ro/drilldown");
   }
 }
 
@@ -315,7 +397,7 @@ export default class Home extends Vue {
   transition: background-image 0.3s ease;
   height: 100%;
   background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url("https://res.cloudinary.com/dv6mzgcxx/image/upload/c_fit,h_1080,w_1920/v1573935364/engine_va4amn.jpg");
+    url("https://res.cloudinary.com/dv6mzgcxx/image/upload/c_scale,w_1920/v1576756182/IMG_5525_clooth.jpg");
   background-repeat: no-repeat;
   background-size: cover;
 
@@ -324,7 +406,7 @@ export default class Home extends Vue {
         from(rgba(0, 0, 0, 0.5)),
         to(rgba(0, 0, 0, 0.8))
       ),
-      url("https://res.cloudinary.com/dv6mzgcxx/image/upload/c_fit,h_1080,w_1920/v1573935364/engine_va4amn.jpg");
+      url("https://res.cloudinary.com/dv6mzgcxx/image/upload/c_scale,w_1920/v1576756182/IMG_5525_clooth.jpg");
   }
 }
 
